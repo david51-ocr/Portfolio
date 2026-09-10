ce70966b8dee57479a83fec44434dc96a725b202
David Mottola <davidaubin02@gmail.com>
2026-09-10 14:35:16 +0200

    aérer a propos

--- src/components/A-Propos.jsx
+++ src/components/A-Propos.jsx
@@ -3,20 +3,34 @@ function APropos() {
     <section id="a-propos" aria-labelledby="titre-aPropos">
       <h2 id="titre-aPropos">À propos</h2>
       <p>
-        Je m'appelle David et je suis développeur web spécialisé dans le front-end React. En parallèle, j'exerce le métier d'éducateur spécialisé,
-         un parcours qui peut sembler éloigné du développement web mais qui m'apporte des compétences 
-         directement utiles dans ma manière de concevoir et de mener un projet.
-Mon expérience dans le secteur social m'a appris à écouter, à comprendre des besoins parfois complexes et à adapter mes réponses à chaque situation. 
-Je retrouve cette même démarche dans le développement web : avant de penser au code, il est essentiel de comprendre le besoin d'un client 
-et les attentes des futurs utilisateurs afin de proposer une solution réellement adaptée.
-Le métier d'éducateur spécialisé m'a également appris à faire face aux imprévus, à analyser 
-un problème et à chercher des solutions lorsqu'une première approche ne fonctionne pas. 
-Ce sont des compétences que j'utilise quotidiennement en développement, notamment lorsque 
-je dois identifier l'origine d'un bug, tester différentes solutions ou apprendre une nouvelle technologie.
-Je souhaite aujourd'hui développer une activité de développeur web freelance en parallèle de mon métier d'éducateur spécialisé. 
-Mon objectif est de réunir ces deux univers en mettant mes compétences techniques au service de projets dans lesquels l'écoute, l'accessibilité et 
-l'expérience utilisateur occupent une place importante.
-      </p>
+  Je m'appelle David et je suis développeur web spécialisé dans le front-end React.
+  En parallèle, j'exerce le métier d'éducateur spécialisé, un parcours qui peut
+  sembler éloigné du développement web mais qui m'apporte des compétences directement
+  utiles dans ma manière de concevoir et de mener un projet.
+</p>
+
+<p>
+  Mon expérience dans le secteur social m'a appris à écouter, à comprendre des besoins
+  parfois complexes et à adapter mes réponses à chaque situation. Je retrouve cette
+  même démarche dans le développement web : avant de penser au code, il est essentiel
+  de comprendre le besoin d'un client et les attentes des futurs utilisateurs afin de
+  proposer une solution réellement adaptée.
+</p>
+
+<p>
+  Le métier d'éducateur spécialisé m'a également appris à faire face aux imprévus,
+  à analyser un problème et à chercher des solutions lorsqu'une première approche ne
+  fonctionne pas. Ce sont des compétences que j'utilise quotidiennement en développement,
+  notamment lorsque je dois identifier l'origine d'un bug, tester différentes solutions
+  ou apprendre une nouvelle technologie.
+</p>
+
+<p>
+  Je souhaite aujourd'hui développer une activité de développeur web freelance en
+  parallèle de mon métier d'éducateur spécialisé. Mon objectif est de réunir ces deux
+  univers en mettant mes compétences techniques au service de projets dans lesquels
+  l'écoute, l'accessibilité et l'expérience utilisateur occupent une place importante.
+</p>
     </section>
   );
 }
